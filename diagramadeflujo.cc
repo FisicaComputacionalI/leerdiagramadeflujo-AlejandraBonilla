2afd68556d4a97eda6796a64e649286ea2b24a72
AlejandraBonilla <31254521+AlejandraBonilla@users.noreply.github.com>
2017-10-23 23:27:05 -0500

    Create diagramadeflujo.cc
    1) Inicio
    2) Defino nuestra variable, s=12
    3) Introduce el valor p. 
    4)Verifico. ¿ p es par? 
      4.1) Sí, p=p+1 pasa al punto 5)
      4.2) No, pasa directo al punto 5)
    5) Verifico, ¿p es primo?
      5.1) Sí, pasa al punto 6)
      5.2) No, p=p+2 y regresa al punto 4)
    6) Verifico, ¿p<s?
      6.1) Sí, s=s-p y regresa al punto 5.2) 
      6.2) No, s=s-1
    7) Verifico, ¿s=0?
      7.1) Sí, introduce el valor de p. 
      7.2) No, regresa al punto 5.2) 
    8) Fin.

---
+++ diagramadeflujo.cc
@@ -0,0 +1,37 @@
+\\Tarea Diagrama de Flujo 
+\\Alejandra Bonilla
+
+#include<iostream>
+using namespace std; 
+int main(){
+  int s=12; 
+  int p=0 
+  cout<<"Dame un numero entero"<<endl; 
+  cin>>p; 
+  
+  if(p is even)
+    {
+      printf(p=p+1); 
+  else 
+      if (p is prime) 
+          {
+            if(p<s) 
+              {while(p<s)
+               {s=s-p; 
+                p=p+2; 
+                return p is prime;  
+            else
+              printf(s=s-1);
+         else
+          printf(p=p+2);
+          return p is prime; 
+        if(s=0)
+          {cout<<p<<endl;
+        else 
+          printf(p=p+2);
+          return p is prime;}
+ return 0;} 
+        
+     
+        
+    

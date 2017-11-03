1665b214d789ff7a07576c925b40db1039af4d70
AlejandraBonilla <31254521+AlejandraBonilla@users.noreply.github.com>
2017-11-02 22:43:01 -0600

    Update diagramadeflujo.cc
    .

--- diagramadeflujo.cc
+++ diagramadeflujo.cc
@@ -2,36 +2,55 @@
 \\Alejandra Bonilla
 
 #include<iostream>
-using namespace std; 
+using namespace std;
+
+int esprimo(int x){
+	int flag=0;
+	for(int i=2; i<x;i++){
+		if (x%i==0)
+		flag=1;
+	}
+	return flag;}
+
 int main(){
-  int s=12; 
-  int p=0 
-  cout<<"Dame un numero entero"<<endl; 
-  cin>>p; 
-  
-  if(p is even)
-    {
-      printf(p=p+1); 
-  else 
-      if (p is prime) 
-          {
-            if(p<s) 
-              {while(p<s)
-               {s=s-p; 
-                p=p+2; 
-                return p is prime;  
-            else
-              printf(s=s-1);
-         else
-          printf(p=p+2);
-          return p is prime; 
-        if(s=0)
-          {cout<<p<<endl;
-        else 
-          printf(p=p+2);
-          return p is prime;}
- return 0;} 
-        
+
+int s=12;
+int p=0;
+int q=1;
+cout<<"Introduce un Numero"<<endl;
+cin>>p;
+do{
+	do{
+		if(p%2==0)
+		p=p+1;
+		cout<<"p="<<p<<endl;
+		if(esprimo(p)==1)
+		p=p+2;
+		cout<<"p="<<p<<endl;
+		}
+	while(esprimo(p)==1);
+
+	if(esprimo(p)==0);
+		if(p<s){
+			s=s-p;
+		}
+		else 
+			s=s-1;
+			cout<<"s="<<s<<endl;	
+		if (s==0){
+		break;
+		}
+		else{
+		p=p+2;
+	}
+	cout<<"p="<<p<<endl;
+}
+
+	while(q=1);
+	
+cout<<"Tu numero final es\n"<<p<<endl;
+return 0;
+}
      
         
     
